ddcffb474f04e2e8566bd07efaa83669ce922145
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2025-01-26 18:24:58 +0900

    static export の設定を追加

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["raw.githubusercontent.com"],
+    // domains: ["raw.githubusercontent.com"],
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
   },
+
+  output: "export",
 };
 
 export default nextConfig;
